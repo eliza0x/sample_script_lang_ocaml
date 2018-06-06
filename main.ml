52eec983f182619490c5b1abb30e9dbe825e2049
eliza0x <me@eliza.link>
2018-06-07 00:39:04 +0900

    update: expand files

--- main.ml
+++ main.ml
@@ -1,55 +1,8 @@
 open Core
 open Lexer
-
-type expr =
-    | Num of int         (* number *)
-    | Add of expr * expr (* a + b *)
-[@@deriving show]
-
-let parse tokens =
-    let rec eq_token t' ts = 
-        match (List.hd ts, t') with
-        | (Some (LParen), LParen) -> Some (List.tl_exn ts)
-        | (Some (RParen), RParen) -> Some (List.tl_exn ts)
-        | (Some (Op op),  Op op') -> Some (List.tl_exn ts)
-        | (Some (Num n),  Num n') -> Some (List.tl_exn ts)
-        | _                -> None
-
-    and expr_parser ts = 
-        let    result  = add_parser ts
-        in let result' = term_parser ts
-        in Option.first_some result result'
-
-    and add_parser ts = 
-        let    result   = term_parser ts
-        in let result'  = Option.value_map result  ~default:None ~f:(fun (ts', _) -> eq_token (Lexer.Op "+") ts') 
-        in let result'' = Option.value_map result' ~default:None ~f:(fun ts' -> expr_parser ts')             
-        in match (result, result', result'') with
-        | (Some (_, t), Some _, Some(ts', e)) -> Some (ts', Add (t, e))
-        | _                                    -> None
-
-    and term_parser ts = 
-        let    result  = paren_parser ts
-        in let result' = form_parser ts
-        in Option.first_some result result'
-
-    and paren_parser ts = 
-        let    result   = eq_token Lexer.LParen ts
-        in let result'  = Option.value_map result  ~default:None ~f:(fun ts' -> expr_parser ts')            
-        in let result'' = Option.value_map result' ~default:None ~f:(fun (ts', _) -> eq_token Lexer.RParen ts') 
-        in match (result', result'') with
-        | (Some (_, e), Some ts' ) -> Some (ts', e)
-        | _                        -> None
-
-    and form_parser ts = 
-        match List.hd ts with
-        | Some(Lexer.Num n) -> Some (List.tl_exn ts, Num n)
-        | _                 -> None
-
-    in Option.value_map (expr_parser tokens) ~default:None ~f:(fun (_, result) -> Some(result)) 
+open Parser
 
 let () =
-    let string_from_tokens t = 
-        List.fold_right ~f:(fun a b -> Lexer.show_token a ^ " " ^ b) ~init:"" t
-    in let tokens = Lexer.tokenize "( 1 + 2 ) + 3"
-    in print_string (show_expr (Option.value (parse tokens) ~default:(Num 0)) ^ "\n")
+    let tokens = Lexer.tokenize "( 1 + 2 ) + 3"
+    in let expr = Option.value (Parser.parse tokens) ~default:(Num 0)
+    in print_string (Parser.show_expr expr ^ "\n")
