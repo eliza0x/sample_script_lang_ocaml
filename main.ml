67c11d2f266ecea6a5bc8a55d18794b5323be648
eliza0x <me@eliza.link>
2018-06-07 00:44:51 +0900

    update: eval

--- main.ml
+++ main.ml
@@ -2,7 +2,12 @@ open Core
 open Lexer
 open Parser
 
+let rec eval expr = 
+    match expr with
+    | Parser.Add (t1, t2) -> eval t1 + eval t2
+    | Parser.Num n     -> n
+
 let () =
     let tokens = Lexer.tokenize "( 1 + 2 ) + 3"
     in let expr = Option.value (Parser.parse tokens) ~default:(Num 0)
-    in print_string (Parser.show_expr expr ^ "\n")
+    in print_string (string_of_int (eval expr) ^ "\n")
